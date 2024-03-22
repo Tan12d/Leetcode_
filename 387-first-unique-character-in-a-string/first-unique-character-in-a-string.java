b1eaa599c910afcf1a67db76326b481c8c4d9141
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2024-03-22 23:52:30 +0530

    Time: 4 ms (94.22%) | Memory: 44.3 MB (93.21%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -3,13 +3,15 @@ class Solution {
         if(s.length()==1) return 0;
 
         int freq[] = new int[26];
+
+        char t[] = s.toCharArray();
         
-        for(char i:s.toCharArray())
+        for(char i:t)
         {
         	freq[i-'a']++;
         }
         
-        for(char i:s.toCharArray())
+        for(char i:t)
         {
         	if(freq[i-'a']==1) return s.indexOf(i);
         }
