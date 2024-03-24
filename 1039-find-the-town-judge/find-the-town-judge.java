bd9a838f6ba7db7c4ddde0826830722e7525e036
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2024-03-24 21:46:04 +0530

    Time: 2 ms (99.60%) | Memory: 53.6 MB (64.62%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -6,33 +6,20 @@ class Solution {
 		
 		else if(trust.length==1) return trust[0][1]; 
 		
-		HashSet<Integer> h=new HashSet<>();
-		HashMap<Integer,Integer> h1=new HashMap<>();
+		int a[]=new int[n+1];
+		int b[]=new int[n+1];
 		
-		for(int i=0;i<trust.length;i++)
+		for(int p[]:trust)
 		{
-			h.add(trust[i][0]);
-			
-			if(h1.containsKey(trust[i][1]))
-			{
-				h1.put(trust[i][1],h1.getOrDefault(trust[i][1],0)+1);
-			}
-			
-			else
-			{
-				h1.put(trust[i][1],1);
-			}
+			a[p[0]]++;
+			b[p[1]]++;
 		}
 		
-		int t=0;
-		
-		for(int i=0;i<trust.length;i++)
+		for(int i=1;i<b.length;i++)
 		{
-			if(h1.containsKey(trust[i][1]) && !h.contains(trust[i][1]))
+			if(b[i]==n-1 && a[i]==0)
 			{
-				t=h1.get(trust[i][1]);
-				if(t==n-1)
-					return trust[i][1];
+				return i;
 			}
 		}
 		
