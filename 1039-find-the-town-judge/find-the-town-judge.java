f45b2514c75d5a858746f9e1cf11f46b5f22845f
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2024-03-24 21:46:55 +0530

    Time: 3 ms (60.51%) | Memory: 53.3 MB (73.72%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,11 +1,6 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        if(trust.length==0 && n==1) return n;
-
-        else if(trust.length==0 && n>1) return -1;
-		
-		else if(trust.length==1) return trust[0][1]; 
-		
+        
 		int a[]=new int[n+1];
 		int b[]=new int[n+1];
 		
