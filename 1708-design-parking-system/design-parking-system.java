3e2742a6a2b11576edfb1289b410cffa543faf6b
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2024-03-24 12:41:42 +0530

    Time: 8 ms (72.20%) | Memory: 45.1 MB (51.07%) - LeetSync

--- 1708-design-parking-system/design-parking-system.java
+++ 1708-design-parking-system/design-parking-system.java
@@ -1,17 +1,18 @@
 class ParkingSystem {
 
-    int n[] = new int[3];
+    int n[] = new int[4];
 
     public ParkingSystem(int big, int medium, int small) {
-        n[0]=big;
-        n[1]=medium;
-        n[2]=small;
+        n[0]=0;
+        n[1]=big;
+        n[2]=medium;
+        n[3]=small;
     }
     
     public boolean addCar(int carType) {
-        if(n[carType-1]>0)
+        if(n[carType]>0)
         {
-            n[carType-1]--;
+            n[carType]--;
             return true;
         }
 
