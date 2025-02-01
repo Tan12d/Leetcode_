43ab43ad020f64c5975b6bb0a04f186781840c57
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2025-02-01 08:03:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (56.64%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -3,18 +3,28 @@ class Solution {
     {
         if(nums.length==1) return true;
 
-        boolean flag[] = new boolean[nums.length];
+        // Method-1
 
-        int j=0;
+        // boolean flag[] = new boolean[nums.length];
+
+        // int j=0;
         
-        for(int i:nums)
-        {
-            flag[j++] = (i%2 == 0 ? true : false);
-        }
+        // for(int i:nums)
+        // {
+        //     flag[j++] = (i%2 == 0 ? true : false);
+        // }
 
-        for(int i=1;i<flag.length;i++)
+        // for(int i=1;i<flag.length;i++)
+        // {
+        //     if(flag[i]==flag[i-1]) return false;
+        // }
+
+
+        // Method-2
+
+        for(int i=1;i<nums.length;i++)
         {
-            if(flag[i]==flag[i-1]) return false;
+            if(nums[i]%2 == nums[i-1]%2) return false;
         }
 
         return true;
