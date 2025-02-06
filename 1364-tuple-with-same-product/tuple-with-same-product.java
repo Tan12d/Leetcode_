bc0fccf492214096070badefb019935773e61521
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2025-02-06 07:53:26 +0530

    Time: 241 ms (39.74%) | Memory: 68.5 MB (73.08%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.java
+++ 1364-tuple-with-same-product/tuple-with-same-product.java
@@ -31,9 +31,4 @@ class Solution {
 		
 		return val;
     }	
-	
-	// public static int combination(int n, int r) {
-    //     if (r == 0 || r == n) return 1; // Base case
-    //     return combination(n - 1, r - 1) + combination(n - 1, r);
-    // }
 }
