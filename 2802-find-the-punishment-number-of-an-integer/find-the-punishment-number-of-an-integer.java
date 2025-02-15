8364ae4c9a1a0f8f8bce41d41f5d928261166515
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2025-02-15 11:04:39 +0530

    Time: 21 ms (73.35%) | Memory: 43.9 MB (72.73%) - LeetSync

---
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int punishmentNumber(int n) 
+    {
+        int square=0;
+        int val=0;
+
+        for(int i=0;i<=n;i++)
+        {
+            square=i*i;
+
+            if(partition(Integer.toString(square), i, 0)) val+=square;
+        }
+        
+        return val;
+    }
+
+
+    public boolean partition(String squareStr, int target, int index)
+    {
+        if(index==squareStr.length()) return target==0;
+
+        int sum=0;
+        for(int i=index;i<squareStr.length();i++)
+        {
+            sum=sum*10+(squareStr.charAt(i)-'0');
+
+            if(sum>target) return false;
+
+            if(partition(squareStr, target-sum, i+1)) return true;
+        }
+
+        return false;
+    }
+}
