0b883227c543518267b13a13a2b14b3b2ba506b5
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2025-02-17 07:22:21 +0530

    Time: 30 ms (18.96%) | Memory: 45.5 MB (30.37%) - LeetSync

---
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    public int numTilePossibilities(String tiles) 
+    {
+        HashSet<String> res=new HashSet<String>();
+        boolean visited[] =new boolean[tiles.length()];
+        String s="";
+
+        possibleWays(tiles, visited, s, res);
+
+        return res.size();        
+    }
+
+    public void possibleWays(String tiles, boolean visited[], String s, HashSet<String> res)
+    {
+        for(int i=0;i<tiles.length();i++)
+        {
+            if(!visited[i])
+            {
+                s+=tiles.charAt(i);
+                visited[i]=true;
+
+                res.add(s);
+
+                possibleWays(tiles, visited, s, res);
+
+                visited[i]=false;
+                s=s.substring(0, s.length()-1);
+            }
+        }
+    }
+}
