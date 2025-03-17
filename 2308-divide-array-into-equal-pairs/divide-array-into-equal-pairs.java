09205ee90f807d282c30845dd08842a32f2c589f
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2025-03-17 07:14:43 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (58.82%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
@@ -1,26 +1,18 @@
 class Solution {
     public boolean divideArray(int[] nums) 
     {
-        HashMap<Integer, Integer> freq = new HashMap<Integer, Integer>();
-		
+        int freq[] = new int[501];		
+
 		for(int i=0;i<nums.length;i++)
 		{
-			if(!freq.containsKey(nums[i]))
-			{
-				freq.put(nums[i], 1);
-			}
-			
-			else
-			{
-				freq.put(nums[i], freq.getOrDefault(nums[i], 0)+1);
-			}
+			freq[nums[i]]^=1;
 		}
 		
-		for(Integer i: freq.values())
+		for(int i=0;i<freq.length;i++)
 		{
-			if(i%2!=0) return false;
+			if((freq[i]&1)==1) return false;
 		}
 		
-		return true;  
+		return true; 
     }
 }
