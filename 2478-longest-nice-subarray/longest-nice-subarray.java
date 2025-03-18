7f20d7fed43b6a888c2950dcd95586e7c6177aa7
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2025-03-18 07:54:57 +0530

    Time: 3 ms (98.64%) | Memory: 55.9 MB (33.79%) - LeetSync

---
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int longestNiceSubarray(int[] nums) 
+    {
+        int left=0;
+        int maxLen=0;
+        
+        int bitsUnion = 0;
+        
+        int n=nums.length;
+        
+        for(int right=0;right<n;right++)
+        {
+        	int temp = nums[right];
+        	
+        	while((bitsUnion & temp)!=0)
+        	{
+        		int leftEle = nums[left];
+        		bitsUnion&=~leftEle;
+        		
+        		left++;
+        	}
+        	
+        	bitsUnion |= temp;
+        	
+        	maxLen = Math.max(maxLen, right-left+1);
+        }
+        
+        return maxLen;        
+    }
+}
