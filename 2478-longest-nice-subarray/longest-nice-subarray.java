ecb39eafe477cb2236c853a8d368893b673210db
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2025-03-18 09:02:28 +0530

    Time: 3 ms (98.64%) | Memory: 56.2 MB (12.81%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.java
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -8,21 +8,15 @@ class Solution {
 		int n = nums.length;
 		int maxLen = 0;
 		
-		
-		int temp_right=0;
-		int temp_left=0;
-		
 		while(right<n)
 		{ 
-			temp_right = nums[right];
-			curr_sum += temp_right;			
-			xor_sum ^= temp_right;
+			curr_sum += nums[right];			
+			xor_sum ^= nums[right];
 			
 			while(curr_sum!=xor_sum)
 			{
-				temp_left = nums[left];
-				curr_sum -= temp_left;
-				xor_sum ^= temp_left;
+				curr_sum -= nums[left];
+				xor_sum ^= nums[left];
 				
 				left++;
 			}
