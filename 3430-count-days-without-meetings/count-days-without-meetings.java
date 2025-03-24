e8bc90273671613a6e5fe25d3c9b34a863330d69
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2025-03-24 08:01:21 +0530

    Time: 75 ms (15.83%) | Memory: 107.8 MB (6.40%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.java
+++ 3430-count-days-without-meetings/count-days-without-meetings.java
@@ -2,6 +2,8 @@ class Solution {
     public int countDays(int days, int[][] meetings) 
     {
         int n=meetings.length;
+
+        if(n==1) return days-=(meetings[0][1]-meetings[0][0])+1;
 		
 		Arrays.sort(meetings, Comparator.comparing(a -> a[0]));
 		               
